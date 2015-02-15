92ddd7afca05a2c10e193c4c50cbcd2c41e0de2d
Vlad Ureche <vlad.ureche@gmail.com>
2015-02-15 22:54:23 +0100

    Scope tests

---
+++ tests/correctness/resources/tests/scopes-conflicting-repr.scala
@@ -0,0 +1,30 @@
+package test
+
+import ildl._
+
+object GCDTest {
+
+  object IntPairAsLong extends TransformationDescription {
+    type High = (Int, Int)
+    type Repr = Long
+    def toRepr(pair: (Int, Int)): Long @high = ???
+    def fromRepr(l: Long @high): (Int, Int) = ???
+  }
+
+  object IntAsLong extends TransformationDescription {
+    type High = Int
+    type Repr = Long
+    def toRepr(pair: Int): Float @high = ???
+    def fromRepr(l: Float @high): Int = ???
+  }
+
+  adrt(IntPairAsLong) {
+    adrt(IntAsLong) {
+      var n1 = (1, 0)
+      var n2 = 1
+      // should produce an error, even though
+      // both high types are represented as Long:
+      n2 = n1
+    }
+  }
+}
---
+++ tests/correctness/resources/tests/scopes-nested.scala
@@ -0,0 +1,29 @@
+package test
+
+import ildl._
+
+object GCDTest {
+
+  object IntPairAsLong extends TransformationDescription {
+    type High = (Int, Int)
+    type Repr = Long
+    def toRepr(pair: (Int, Int)): Long @high = ???
+    def fromRepr(l: Long @high): (Int, Int) = ???
+  }
+
+  object IntAsLong extends TransformationDescription {
+    type High = Int
+    type Repr = Long
+    def toRepr(pair: Int): Long @high = ???
+    def fromRepr(l: Long @high): Int = ???
+  }
+
+  adrt(IntPairAsLong) {
+    adrt(IntAsLong) {
+      var n1 = (1, 0)
+      var n2 = 1
+      // the signature should take 2 longs and return a long:
+      def foo(n1: (Int, Int), n2: Int): Int = ???
+    }
+  }
+}
