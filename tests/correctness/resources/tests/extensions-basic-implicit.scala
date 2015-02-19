4f3882d1d502a9a3e35ae542537e8ca0751ecbec
Vlad Ureche <vlad.ureche@gmail.com>
2015-02-19 16:19:35 +0100

    Tests for the implicit method invocation opt

---
+++ tests/correctness/resources/tests/extensions-basic-implicit.scala
@@ -0,0 +1,39 @@
+package tests
+
+import ildl._
+
+object Test {
+
+  case class ZZZ(v: Long)
+
+  implicit class ZZZOps(zzz: ZZZ) {
+    def +(other: ZZZ) = ZZZ(zzz.v + other.v)
+    def -(other: ZZZ) = ZZZ(zzz.v - other.v)
+    def *(other: ZZZ) = ZZZ(zzz.v * other.v)
+    def /(other: ZZZ) = ZZZ(zzz.v / other.v)
+    def %(other: ZZZ) = ZZZ(zzz.v % other.v)
+  
+  }
+
+  object ZZZAsLong extends FreestyleTransformationDescription {
+    def toRepr(zzz: ZZZ): Long @high = zzz.v
+    def fromRepr(v: Long @high): ZZZ = ZZZ(v)
+    def implicit_ZZZOps_+(recv: Long @high, other: ZZZ): Long @high = ???        // okay, coercing
+    def implicit_ZZZOps_-(recv: Long @high, other: Any): Long @high = ???        // okay, coercing
+    def implicit_ZZZOps_*(recv: Long @high, other: Long @high): Long @high = ??? // okay, non-coercing
+    def implicit_ZZZOps_/(recv: Long @high, other: String): Long @high = ???     // wrong, other: String
+    def implicit_ZZZOps_%(recv: Long @high, other: Long @high): Long @high = ??? // overloaded
+    def implicit_ZZZOps_%(recv: Long @high, other: Any): Long @high = ???
+    def extension_toString(): String = ???
+  }
+
+  adrt(ZZZAsLong) { 
+    val a = ZZZ(1)
+    val b = a + a // yes, coercing
+    val c = a - a // yes, coercing
+    val d = a * a // yes
+    val e = a / a // no
+    val f = a % a // no
+    f.toString    // no
+  }
+}
---
+++ tests/correctness/resources/tests/extensions-generic-implicit.scala
@@ -0,0 +1,26 @@
+package tests
+
+import ildl._
+
+object Test {
+
+  case class MyList[T](arr: Array[T])
+
+  implicit class MyListOps[T](myl: MyList[T]) {
+    def foo(i: Int) = myl.arr(i)
+    def bar(i: Int) = myl.arr(i)
+  }
+
+  object ListAsArray extends FreestyleTransformationDescription {
+    def toRepr[T](list: MyList[T]): Array[T] @high = ???
+    def fromRepr[T](arr: Array[T] @high): MyList[T] = ???
+    def implicit_MyListOps_foo[T](arr: Array[T] @high, i: Int) = ???     // okay
+    def implicit_MyListOps_foo[T](arr: Array[T] @high, i: String) = ???  // not okay, i: String
+  }
+
+  adrt(ListAsArray) { 
+    val a = MyList(Array(1,2,3))
+    val b = a.foo(2)  // okay
+    val c = a.bar(3)  // not working
+  }
+}
