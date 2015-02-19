bb469992d1775d53ce65c1e38db6dd5b3f55c0b0
Vlad Ureche <vlad.ureche@gmail.com>
2015-02-19 16:20:51 +0100

    Stray test

---
+++ tests/correctness/resources/tests/extensions-generic.scala
@@ -0,0 +1,21 @@
+package tests
+
+import ildl._
+
+object Test {
+
+  object ListAsArray extends FreestyleTransformationDescription {
+    def toRepr[T](list: List[T]): Array[T] @high = ???
+    def fromRepr[T](arr: Array[T] @high): List[T] = ???
+    def extension_apply[T](recv: Array[T] @high, index: Int): T = ??? // okay
+    def extension_toString[T](recv: Array[T] @high): Long = ???       // bad
+    // TODO: This shouldn't work, but it does:
+    //def extension_toString(recv: Array[String] @high): String = ???     // bad
+  }
+
+  adrt(ListAsArray) { 
+    val a = List(1,2,3)
+    val b = a(2)
+    val c = a.toString
+  }
+}
