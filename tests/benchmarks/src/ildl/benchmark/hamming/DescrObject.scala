9e81b38b0aae114ca74c099c8550bf91c0baea2c
Vlad Ureche <vlad.ureche@gmail.com>
2015-05-04 01:29:48 +0200

    Hamming Benchmark

--- tests/benchmarks/src/ildl/benchmark/hamming/DescrObject.scala
+++ tests/benchmarks/src/ildl/benchmark/hamming/DescrObject.scala
@@ -4,6 +4,31 @@ package hamming
 
 import collection.mutable.Queue
 
+object BigIntAsLong extends TransformationDescription {
+  def toRepr(high: BigInt): Long @high = {
+    assert(high.isValidLong)
+    high.longValue()
+  }
+  def toHigh(repr: Long @high): BigInt = BigInt(repr)
+
+  def extension_*(recv: Long @high, other: Long @high): Long @high =
+    // note: Math.multiplyExact requires Java 8
+    // java.lang.Math.multiplyExact(recv, other)
+    recv * other
+
+  def extension_+(recv: Long @high, other: Long @high): Long @high =
+    // note: Math.multiplyExact requires Java 8
+    // java.lang.Math.addExact(recv, other)
+    recv + other
+
+  def extension_min(recv: Long @high, other: Long @high): Long @high =
+    if (recv < other)
+      recv
+    else
+      other
+}
+
+
 object QueueOfLongAsFunnyQueue extends RigidTransformationDescription {
 
   type High = Queue[Long]
@@ -19,16 +44,16 @@ object QueueOfLongAsFunnyQueue extends RigidTransformationDescription {
     ???
   }
 
-  def implicit_QueueWithEnqueue1_enqueue1(q: FunnyQueue @high)(bi: Long): Unit = {
-    q.enqueue(bi.toLong)
+  def implicit_QueueWithEnqueue1_enqueue1(q: FunnyQueue @high)(bi: Long @high(BigIntAsLong)): Unit = {
+    q.enqueue(bi)
   }
 
-  def extension_enqueue(q: FunnyQueue @high)(bi: Long*): Unit = {
+  def extension_enqueue(q: FunnyQueue @high)(bi: Long @high(BigIntAsLong)*): Unit = {
     // we don't support more than one element :)
     assert(bi.size == 1)
     q.enqueue(bi.apply(0))
   }
 
-  def extension_dequeue(q: FunnyQueue @high): Long = q.dequeue()
-  def extension_head(q: FunnyQueue @high): Long = q.head()
+  def extension_dequeue(q: FunnyQueue @high): Long @high(BigIntAsLong) = q.dequeue()
+  def extension_head(q: FunnyQueue @high): Long @high(BigIntAsLong) = q.head()
 }
--- tests/benchmarks/src/ildl/benchmark/hamming/HammingNumbers.scala
+++ tests/benchmarks/src/ildl/benchmark/hamming/HammingNumbers.scala
@@ -12,11 +12,11 @@ object HammingNumbers {
   }
 
   // taken from http://rosettacode.org/wiki/Hamming_numbers#Scala
-  class HammingDirect extends Iterator[Long] {
-    val q2 = new Queue[Long]
-    val q3 = new Queue[Long]
-    val q5 = new Queue[Long]
-    def enqueue(n: Long) = {
+  class HammingDirect extends Iterator[BigInt] {
+    val q2 = new Queue[BigInt]
+    val q3 = new Queue[BigInt]
+    val q5 = new Queue[BigInt]
+    def enqueue(n: BigInt) = {
       q2 enqueue1 n * 2
       q3 enqueue1 n * 3
       q5 enqueue1 n * 5
@@ -35,28 +35,30 @@ object HammingNumbers {
     q5 enqueue 1
   }
 
-  adrt(QueueOfLongAsFunnyQueue) {
-    class HammingADRT extends Iterator[Long] {
-      val q2 = new Queue[Long]
-      val q3 = new Queue[Long]
-      val q5 = new Queue[Long]
-      def enqueue(n: Long) = {
-        q2 enqueue1 n * 2
-        q3 enqueue1 n * 3
-        q5 enqueue1 n * 5
+  adrt(BigIntAsLong) {
+    adrt(QueueOfLongAsFunnyQueue) {
+      class HammingADRT extends Iterator[BigInt] {
+        val q2 = new Queue[BigInt]
+        val q3 = new Queue[BigInt]
+        val q5 = new Queue[BigInt]
+        def enqueue(n: BigInt) = {
+          q2 enqueue1 n * 2
+          q3 enqueue1 n * 3
+          q5 enqueue1 n * 5
+        }
+        def next = {
+          val n = q2.head min q3.head min q5.head
+          if (q2.head == n) q2.dequeue
+          if (q3.head == n) q3.dequeue
+          if (q5.head == n) q5.dequeue
+          enqueue(n)
+          n
+        }
+        def hasNext = true
+        q2 enqueue 1
+        q3 enqueue 1
+        q5 enqueue 1
       }
-      def next = {
-        val n = q2.head min q3.head min q5.head
-        if (q2.head == n) q2.dequeue
-        if (q3.head == n) q3.dequeue
-        if (q5.head == n) q5.dequeue
-        enqueue(n)
-        n
-      }
-      def hasNext = true
-      q2 enqueue 1
-      q3 enqueue 1
-      q5 enqueue 1
     }
   }
 }
