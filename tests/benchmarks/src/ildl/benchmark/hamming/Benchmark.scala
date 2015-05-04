5c9ce2f4f0967298ad66183e314f65d003d52ddc
Vlad Ureche <vlad.ureche@gmail.com>
2015-05-04 03:44:20 +0200

    More twix

--- tests/benchmarks/src/ildl/benchmark/hamming/Benchmark.scala
+++ tests/benchmarks/src/ildl/benchmark/hamming/Benchmark.scala
@@ -28,18 +28,16 @@ object BenchmarkRunner extends PerformanceTest.Microbenchmark {
           exec.jvmflags -> flags(interp)
       ) setUp {
         _ =>
-          val r1 = BigInt("8100000000")
-          val r2 = (new HammingDirect().drop(2000)).next()
-          val r3 = (new HammingADRT().drop(2000)).next()
-          assert(r2 == r1, r2.toString)
-          assert(r3 == r1, r3.toString)
+          val r1 = (new HammingDirect().drop(10000)).next()
+          val r2 = (new HammingADRT().drop(10000)).next()
+          assert(r1 == r2, r1.toString + " vs " + r2.toString)
       } in {
         case (bench, _) =>
           bench match {
             case "direct" =>
-              (new HammingDirect().drop(100000)).next()
+              (new HammingDirect().drop(10000)).next()
             case "adrt__" =>
-              (new HammingADRT().drop(100000)).next()
+              (new HammingADRT().drop(10000)).next()
           }
       }
     }
