5e00fcaca1872032e9c5f83198ed74b0a771e7d4
Vlad Ureche <vlad.ureche@gmail.com>
2015-05-08 05:45:11 +0200

    Get GCD up to date

--- tests/benchmarks/src/ildl/benchmark/gcd/DescrObject.scala
+++ tests/benchmarks/src/ildl/benchmark/gcd/DescrObject.scala
@@ -2,13 +2,14 @@ package ildl
 package benchmark
 package gcd
 
-// http://mathforum.org/library/drmath/view/67068.html
-object IntPairAsGaussianInteger extends RigidTransformationDescription {
-
-  type High = (Int, Int)
-  type Repr = Long
-
-  private def pack(re: Int, im: Int) = (re.toLong << 32l) | (im.toLong & 0xFFFFFFFFl)
+/**
+ *  A transformation description object which can encode Gaussian integers,
+ *  modelled as pairs of integers (`(Int, Int)`) as Long integers (`Long`)
+ *  which are automatically unboxed by the Scala backend.
+ *
+ *  @see [[http://mathforum.org/library/drmath/view/67068.html]]
+ */
+object IntPairAsGaussianInteger extends TransformationDescription {
 
   // coercions:
   def toRepr(pair: (Int, Int)): Long @high = pack(pair._1, pair._2)
@@ -20,8 +21,11 @@ object IntPairAsGaussianInteger extends RigidTransformationDescription {
   // interface: (no need to expose everything)
   def implicit_IntPairAsGaussianIntegerImplicit_%(n1: Long @high, n2: Long @high): Long @high = %(n1, n2)
   def implicit_IntPairAsGaussianIntegerImplicit_norm(n: Long @high): Int = norm(n)
+  // TODO: Add the interface methods here, to avoid converting
+  // `Long` back into `(Int, Int)` to execute operatiors...
 
   // implementation:
+  private def pack(re: Int, im: Int) = (re.toLong << 32l) | (im.toLong & 0xFFFFFFFFl)
   private def re(l: Long @high): Int = (l >>> 32).toInt
   private def im(l: Long @high): Int = (l & 0xFFFFFFFF).toInt
   private def norm(l: Long @high): Int = re(l)^2 + im(l)^2
@@ -36,7 +40,7 @@ object IntPairAsGaussianInteger extends RigidTransformationDescription {
     assert(im(denom) == 0)
     pack(math.round(re(numer).toFloat / re(denom)), math.round(im(numer).toFloat / re(denom)))
   }
-  private def %(n1: Long @high, n2: Long @high): Long @high = pos(IntPairAsGaussianInteger.-(n1,*(/(n1, n2), n2)))
+  private def %(n1: Long @high, n2: Long @high): Long @high = pos(this.-(n1,*(/(n1, n2), n2)))
   private def pos(n1: Long @high): Long @high = if (re(n1) < 0) pack(-re(n1), -im(n1)) else n1
   private def bits(n1: Long @high): String = f"$n1%016x"
 }
