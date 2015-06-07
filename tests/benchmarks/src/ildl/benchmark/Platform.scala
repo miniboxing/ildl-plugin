e399332e3d8a8121cb1e7b378d722dfde4401a52
Vlad Ureche <vlad.ureche@gmail.com>
2015-06-08 00:28:39 +0200

    Platform twix

--- tests/benchmarks/src/ildl/benchmark/Platform.scala
+++ tests/benchmarks/src/ildl/benchmark/Platform.scala
@@ -5,7 +5,17 @@ object Platform {
 
   def checkCompatibility() = {
     // x86 architecture
-    assert(sys.props.getOrElse("os.arch", "") == "amd64", "Unsupported architecture:" + sys.props("os.arch"))
+    assert(sys.props.getOrElse("os.arch", "") == "amd64" ||
+           sys.props.getOrElse("os.arch", "") == "i386", "Unsupported architecture:" + sys.props("os.arch"))
+
+    if (sys.props.getOrElse("os.arch", "") == "i386") {
+      println()
+      println()
+      println("WARNING!")
+      println("The benchmarks may be affected by the fact that we are running on a 32-bit operating system!")
+      println()
+      println()
+    }
 
     // Java version
     assert(sys.props.getOrElse("java.version", "").startsWith("1.7"), "Incorrect Java version:" + sys.props("java.version"))
